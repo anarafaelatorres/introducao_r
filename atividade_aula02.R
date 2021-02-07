afcec7e9321b20cc56a43e919262ac32e345cec0
Ana Rafaela Torres <57814984+anarafaelatorres@users.noreply.github.com>
2021-02-07 18:54:08 -0300

    introducao_r_aula_02

--- atividade_aula02.R
+++ atividade_aula02.R
@@ -1,30 +1,30 @@
-# Exercícios - Introdução ao R
+# ExercÃ­cios - IntroduÃ§Ã£o ao R
 
 # objetos -----------------------------------------------------------------
 
 # (a) Crie um vetor com o nome de tres frutas e guarde em um objeto chamado frutas.
   frutas<-c('banana','uva', 'laranja')
 
-# (b) Use a função length() para contar o tamanho do vetor. Certifique-se que retorna 3.
+# (b) Use a funÃ§Ã£o length() para contar o tamanho do vetor. Certifique-se que retorna 3.
   length(frutas)
 
 # (c) Use [] para retornar a primeira fruta do vetor.
   frutas[1]
 
-# (d) Inspecione a saída de 'paste("eu gosto de", frutas)' e responda se o tamanho do vetor mudou.
+# (d) Inspecione a saÃ­da de 'paste("eu gosto de", frutas)' e responda se o tamanho do vetor mudou.
   paste("eu gosto de ", frutas)
   length(paste("eu gosto de ", frutas))
-  #' O tamanho do vetor não mudou
+  #' O tamanho do vetor nÃ£o mudou
 
 # vetores e funcoes ---------------------------------------------------------
 
-# 1. Analise as duas linhas de códigos abaixo:
-c(1,2,3) - 1           # código 1
-c(1,2,3) - c(1,1,1)    # código 2
+# 1. Analise as duas linhas de cÃ³digos abaixo:
+c(1,2,3) - 1           # cÃ³digo 1
+c(1,2,3) - c(1,1,1)    # cÃ³digo 2
 
-# Os resultados são iguais? Porquê?
+# Os resultados sÃ£o iguais? PorquÃª?
 
-  #'São iguais, no primeiro caso acontece a reciclagem,ou seja,  no primeiro caso o r ira subtrair de cada elemento do vetor 1 o numero 1
-  #'já no segundo caso esta subtraindo o primeiro elemnto do vetor 1 com o primeiro elemento do vetor 2
-  #'mas como no segundo vetor todos os elementos são iguais a 1
-  #'então o codigo 1 e o codigo 2 retornam o mesmo resultado.
+  #'SÃ£o iguais, no primeiro caso acontece a reciclagem,ou seja,  no primeiro caso o r ira subtrair de cada elemento do vetor 1 o numero 1
+  #'jÃ¡ no segundo caso esta subtraindo o primeiro elemnto do vetor 1 com o primeiro elemento do vetor 2
+  #'mas como no segundo vetor todos os elementos sÃ£o iguais a 1
+  #'entÃ£o o codigo 1 e o codigo 2 retornam o mesmo resultado.
